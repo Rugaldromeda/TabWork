2053086eb61a61db0f4b19af099aa9db5d97a765
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-06-11 16:16:01 -0300

    Atualizando

--- app/controller/worksheetController.js
+++ app/controller/worksheetController.js
@@ -2,11 +2,11 @@
 import { ListInputs } from "../model/ListInputs.js";
 import { ModelInputs } from "../model/ModelInputs.js";
 
-export class worksheetController {
+export class worksheetControl {
     constructor(worksheetController){
         const $ = document.querySelector.bind(document)
         // use uma classe, pra essa forma de seleção é melhor. 
-        const formController = $(`[data-controller-${worksheetController.replace(/\s+/g, '').replace(/\.+/g , '').toLowerCase()}]`)
+        this.formControllername = $(`[data-controller-${worksheetController.replace(/\s+/g, '').replace(/\.+/g , '').toLowerCase()}]`);
         this.nameInput;
         this.typeInput;
 
@@ -15,10 +15,12 @@ export class worksheetController {
     }
 
     add(){
-        this.nameInput = formController.querySelector('[data-name-input]');
-        this.typeInput = formController.querySelector('[data-type-input]');
-        this._listInputs.add(this._createInput());
-        this._worksheetView.update(this._listWorksheet);
+        
+        //formController.addEventListenner("submit", (event) => event.preventDefault())
+        //this.nameInput = formController.querySelector('[data-name-input]');
+        //this.typeInput = formController.querySelector('[data-type-input]');
+        //this._listInputs.add(this._createInput());
+        //this._worksheetView.update(this._listWorksheet);
         
     }
 
