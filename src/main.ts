92bd4d4d54b492523772af00a0ea48e6825eec73
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-07-07 16:00:41 -0300

    Integrando store, key e router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import store from './store'
+import router from './routes'
+import { key, store } from './store'
 
-createApp(App).use(store).mount('#app')
+createApp(App).use(router).use(store,key).mount('#app')
