1d21e097891eea2a57a5a901de5a2ae9258e3bfc
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-07-07 15:55:05 -0300

    Componente com formulário que cria um campo tabwork

---
+++ src/components/Createsheet.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+        <label for="nameSheet">Nome Tabela</label>
+        <input type="text" id="nameSheet">
+
+        <select id="modelSheet" >
+            <option value="">Selecione o modelo</option>
+            <option :value="modelsheet.id" v-for="modelsheet in modelsheets" :key="modelsheet.id"> {{modelsheet.model}}</option>
+        </select>
+    </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { key } from "@/store";
+import { useStore } from "vuex";
+import { computed } from "@vue/reactivity";
+
+export default defineComponent({
+    name: 'CreateSheet',
+    data() {
+        return{
+            idModelsheet: ''
+        }
+    },
+    setup(){
+        const store = useStore(key)
+        return{
+            modelsheets: computed(()=> store.state.modelsheets)
+        }
+    }
+})
+</script>
