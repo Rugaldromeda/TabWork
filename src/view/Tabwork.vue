32c2498725ae53a442fc5b6a097559ab97e1f591
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-07-09 13:21:37 -0300

    Criando campo view

---
+++ src/view/Tabwork.vue
@@ -0,0 +1,21 @@
+<template>
+    <Createtabwork @toSaveTabwork="showTabworks"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Createtabwork from "@/components/Createtabwork.vue";
+import ITabwork from "@/interfaces/ITabwork";
+
+export default defineComponent({
+    name: "TabworkView",
+    components:{
+        Createtabwork
+    },
+    methods:{
+        showTabworks(tabwork:ITabwork){
+            console.log(tabwork)
+        }
+    }
+})
+</script>
