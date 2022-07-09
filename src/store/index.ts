77c2753e5e357ee62ac88ed4869a28f9b06226c4
Gabriel Lima <69268986+Rugaldromeda@users.noreply.github.com>
2022-07-09 13:17:52 -0300

    Criando mutations add tabwork

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { createStore, Store, useStore as vuexUseStore } from 'vuex'
 import IModelsheet from '@/interfaces/IModelsheet'
 import { InjectionKey } from 'vue'
+import ITabwork from '@/interfaces/ITabwork';
 
 interface State {
-  modelsheets: IModelsheet[]
+  modelsheets: IModelsheet[],
+  tabworks: ITabwork[]
 }
 
 export const key: InjectionKey<Store<State>> = Symbol();
@@ -12,14 +14,18 @@ export const store = createStore<State>({
   state: {
     modelsheets: [
       {
-        id:'1',
+        id:"1",
         model:'Orçamento e Balanço'
       }
-    ]
+    ],
+    tabworks:[]
   },
   getters: {
   },
   mutations: {
+    'ADD_TABWORK'(state, tabwork: ITabwork ){
+      state.tabworks.push(tabwork)
+    }
   },
   actions: {
   },
